fbd17365ea2718b776bca4c6b6886d181d12adb0
Minghao PAN <MPan2@slb.com>
2020-04-08 22:59:15 +0800

    jwt auth demo

---
+++ Controllers/JwtAuthController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JwtAuthDemo.Models;
+using JwtAuthDemo.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtAuthDemo.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class JwtAuthController : ControllerBase
+    {
+        private readonly IJwtAuthService _jwtAuthService;
+
+        public JwtAuthController(IJwtAuthService jwtAuthService)
+        {
+            _jwtAuthService = jwtAuthService;
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Authenticate([FromBody] UserCred userCred)
+        {
+            var token = _jwtAuthService.Authenticate(userCred.Username, userCred.Password);
+
+            if (token == null)
+                return Unauthorized();
+
+            return Ok(token);
+        }
+    }
+}
---
+++ Services/JwtAuthService.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JwtAuthDemo.Services
+{
+    public class JwtAuthService : IJwtAuthService
+    {
+        private readonly byte[] _tokenKey;
+
+        private readonly IDictionary<string, string> _users = new Dictionary<string, string>
+        {
+            { "test123", "123" },
+            { "test234", "234" }
+        };
+
+        public JwtAuthService(byte[] tokenKey)
+        {
+            _tokenKey = tokenKey;
+        }
+
+        public string Authenticate(string username, string password)
+        {
+            if (!_users.Any(u => u.Key == username && u.Value == password))
+            {
+                return null;
+            }
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, username)
+                }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(_tokenKey),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
+    }
+}
